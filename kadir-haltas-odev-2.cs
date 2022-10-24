db443baf65232fba0e609895c1c5e53bd3aa369c
mert <57325062+valtair0@users.noreply.github.com>
2022-10-24 20:57:12 +0300

    Create kadir-haltas-odev-2.cs

---
+++ kadir-haltas-odev-2.cs
@@ -0,0 +1,64 @@
+using System;
+
+namespace Odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //ÖDEVİN ÇÖZÜLMEMİŞ HALİ
+           /*
+            Console.Write("Sayıyı Giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i < sayi; i++)
+            {
+
+                if (sayi%i==0)
+                {
+                    Console.Write(i+",");
+                }
+
+            }
+
+            Console.ReadLine();
+            */
+            //---------------------------------------------------
+
+
+
+           //ÇÖZÜMÜ
+            Console.Write("Sayıyı Giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int sonBolunenSayi=0;
+
+            for (int i = sayi-1; i <= sayi; i--)
+            {
+                    if (sayi%i==0)
+                    {
+                        sonBolunenSayi = i;
+                        break;
+                    }
+                  
+            }
+
+
+            for (int i = 2; i < sayi; i++)
+            {
+
+                if (sayi%i==0)
+                {
+                    Console.Write(i);
+                    if (i!=sonBolunenSayi)
+                    {
+                        Console.Write(",");
+                    }
+                }
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
